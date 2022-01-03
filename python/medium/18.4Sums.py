d3097515079b333fdeddbdde9f61f82e5a03fd09
Serge A <serge.ahranovich@gmail.com>
2022-01-03 14:59:12 -0800

    correct solution python 18

--- python/medium/18.4Sums.py
+++ python/medium/18.4Sums.py
@@ -2,41 +2,61 @@ from typing import List
 from itertools import combinations
 from utils import task18, task18res, testtime
 
-CNT = 100
+CNT = 1000
 # @lc code=start
 
 
 class Solution:
-    # @testtime(CNT)
-    # def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-    #     res = set()
-    #     for k in combinations(nums, 4):
-    #         if sum(k) == target:
-    #             res.add(tuple(sorted(list(k))))
-    #     return list(res)
-
     @testtime(CNT)
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums, ln, res = sorted(nums), len(nums), []
-        for k in range(0, ln - 3):
-            if k and nums[k] == nums[k - 1]:
-                continue
-            for i in range(k + 1, ln - 2):
-                if i > k + 1 and nums[i] == nums[i - 1]:
-                    continue
-                l, r, sm = i + 1, ln - 1, 1
-                while l < r:
-                    sm = nums[i] + nums[l] + nums[r] + nums[k]
-                    if sm > target:
-                        r -= 1
-                    elif sm < target:
-                        l += 1
-                    else:
-                        res.append([nums[i], nums[l], nums[r], nums[k]])
-                        l += 1
-                        while l < r and nums[l] == nums[l - 1]:
-                            l += 1
-        return res
+        def kSum(nums: List[int], target: int, k: int) -> List[List[int]]:
+            res = []
+
+            # If we have run out of numbers to add, return res.
+            if not nums:
+                return res
+
+            # There are k remaining values to add to the sum. The
+            # average of these values is at least target // k.
+            average_value = target // k
+
+            # We cannot obtain a sum of target if the smallest value
+            # in nums is greater than target // k or if the largest
+            # value in nums is smaller than target // k.
+            if average_value < nums[0] or nums[-1] < average_value:
+                return res
+
+            if k == 2:
+                return twoSum(nums, target)
+
+            for i in range(len(nums)):
+                if i == 0 or nums[i - 1] != nums[i]:
+                    for subset in kSum(nums[i + 1 :], target - nums[i], k - 1):
+                        res.append([nums[i]] + subset)
+
+            return res
+
+        def twoSum(nums: List[int], target: int) -> List[List[int]]:
+            res = []
+            lo, hi = 0, len(nums) - 1
+
+            while lo < hi:
+                curr_sum = nums[lo] + nums[hi]
+                if curr_sum < target or (lo > 0 and nums[lo] == nums[lo - 1]):
+                    lo += 1
+                elif curr_sum > target or (
+                    hi < len(nums) - 1 and nums[hi] == nums[hi + 1]
+                ):
+                    hi -= 1
+                else:
+                    res.append([nums[lo], nums[hi]])
+                    lo += 1
+                    hi -= 1
+
+            return res
+
+        nums.sort()
+        return kSum(nums, target, 4)
 
 
 # @lc code=end
@@ -48,4 +68,5 @@ print(
 
 print(Solution().fourSum([1, 0, -1, 0, -2, 2], 0))
 print(Solution().fourSum([2, 2, 2, 2], 8))
+print(Solution().fourSum(task18, task18res))
 print(Solution().fourSum(task18, task18res))
