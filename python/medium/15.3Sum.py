2f5a1e4c78fe8ba7dc2af2c8f76123c1eb630768
Serge A <serge.ahranovich@gmail.com>
2022-01-02 19:46:57 -0800

    python 15

--- python/medium/15.3Sum.py
+++ python/medium/15.3Sum.py
@@ -1,14 +1,49 @@
 from typing import List
+from utils import task15array, testtime
 
 # @lc code=start
 
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        pass
+        nums, ln, res = sorted(nums), len(nums), []
+        for i in range(0, ln - 2):
+            if i and nums[i] == nums[i - 1]:
+                continue
+            l, r, sm = i + 1, ln - 1, 1
+            while l < r:
+                if l > i + 1 and nums[l] == nums[l - 1]:
+                    l += 1
+                    continue
+                if r < ln - 1 and nums[r] == nums[r + 1]:
+                    r -= 1
+                    continue
+                sm = nums[i] + nums[l] + nums[r]
+                if sm == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    r -= 1
+                elif sm > 0:
+                    r -= 1
+                else:
+                    l += 1
+        return res
 
 
 # @lc code=end
 
-t = [-1, 0, 1, 2, -1, -4]
-print(Solution().threeSum(t))
+# t = [-1, 0, 1, 2, -1, -4, -1, 0, 1, 2, -1, -4, -1, 0, 1, 2, -1, -4, -1, 0, 1]
+t = [-4, -4, -4, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2]
+t = [-4, -1, -1, 0, 1, 2]
+
+
+@testtime(10)
+def tst(t):
+    return Solution().threeSum(t)
+
+
+tst(task15array)
+
+print(tst([0, 0, 0, 0]))
+print(tst(t))
+
+# print(Solution().threeSum(task15array))
